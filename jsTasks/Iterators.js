e01505b725fed231343d0e29abfe57e863ba0ef1
Harry Aston <harrylaston@googlemail.com>
2021-01-21 16:24:16 +0000

    iterator Js work

---
+++ jsTasks/Iterators.js
@@ -0,0 +1,44 @@
+// assigning functions to variables
+const addFive = number => {
+    return number + 5;
+}
+
+let f = addFive;
+
+console.log(addFive(8));
+
+//higher order functions (functions that take other functions as arguments)
+let addFiveTimesThree = (f, number) => {
+    let aF = f(number);
+    return aF * 3;
+}
+
+let aFTT = addFiveTimesThree;
+let test = aFTT(f, 8);
+
+// Arrays for each
+
+let array1 = [5, 10, 15, 20]
+
+array1.forEach(value => {
+    let aFTTValues = aFTT(f, value)
+    console.log(aFTTValues);
+});
+
+//Arrays filter 
+
+let filteredArray = array1.filter(value => {
+    return value <= 10;
+});
+
+console.log(filteredArray);
+
+//Arrays Map, this well execute the code for each iteration in the array and push the returned value into a new array
+let array3 = ['harry', 'jack', 'ryan']
+
+let mappedArray = array3.map(value => {
+    return value + " is now online";
+})
+
+console.log(mappedArray);
+
